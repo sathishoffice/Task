d7de4a0fa187bb9e142b43dd77e4da2a334281c9
sathishkumar <sathish91@macappstudio.com>
2021-10-19 17:38:32 +0530

    name

--- app/src/main/java/com/project/FlendzzTask/ApiInterface.kt
+++ app/src/main/java/com/project/FlendzzTask/ApiInterface.kt
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit
 
 interface ApiInterface {
 
-    //Shaahid
+    //Sathish
     @POST("serviceDetails.php")
     fun list(@Header("Authorization") token: String,
              @Body params: JsonObject
